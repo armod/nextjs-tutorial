7125ab6d7d0ce0c1019315221dcc868371fd93e4
armod <armods@gmail.com>
2025-02-20 14:04:03 +0100

    update

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>HomePage</h1>
+      <Link href='/about' className='text-2xl'>
+        about page
+      </Link>
     </div>
   )
 }
